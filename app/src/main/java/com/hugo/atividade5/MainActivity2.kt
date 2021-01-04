e9381de1bd3e3d39ee4d652dfe55a4f3e8d70b6c
hugocrs <60903030+hugocrs@users.noreply.github.com>
2021-01-04 15:32:13 -0300

    Initial commit

---
+++ app/src/main/java/com/hugo/atividade5/MainActivity2.kt
@@ -0,0 +1,17 @@
+package com.hugo.atividade5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val i = intent
+        val nome = i.getStringExtra("nome")
+        campo.text = nome
+
+    }
+}
